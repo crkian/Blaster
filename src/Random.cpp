de0449b13157ce2713542fc03d37eb1eea18b8d5
crkian <blogsrecord@googlemail.com>
2015-04-08 20:16:16 +0100

    Altered aliens, ship and asteroids

--- src/Random.cpp
+++
@@ -1,6 +0,0 @@
-#include "Random.h"
-#include <stdlib.h>
-int Random::Int(int lower, int upper)
-{
-return rand()%(upper-lower+1) + lower;
-}
--- src/Random.h
+++
@@ -1,8 +0,0 @@
-#ifndef RANDOM_H
-#define RANDOM_H
-class Random
-{
-public:
-static int Int(int lower, int upper);
-};
-#endif
